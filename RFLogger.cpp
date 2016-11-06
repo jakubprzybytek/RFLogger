22828d435a10e087a10dffe6dec6046e8b752c4e
The Developer <cubanan@gmail.com>
2016-11-06 18:10:09 +0000

    Added params support

--- RFLogger.cpp
+++ RFLogger.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
+#include <getopt.h>
 
 using namespace std;
 
 #include "SDRDevice.hpp"
 
-int main () {
+static void printHelp() {
+    cout << "Usage RFLogger [options]" << endl;
+    cout << "  Options summary:" << endl;
+    cout << "    -l, --list-device		List all available SDR devices and exit" << endl;
+    cout << "    -i, --device-info		Display detailed information about the device in use" << endl;
+    cout << "    -h, --help			Display this help information and exit" << endl;
+}
+
+int main (int argc, char **argv) {
+
+    static int printDeviceInfo = 0;
+
     cout << "Starting..." << endl;
 
-//    SDRDevice::listAvailableSDRDevices();
+    static struct option longOptions[] = {
+	{"list-devices", no_argument, 0, 'l'},
+	{"device-info", no_argument, &printDeviceInfo, 'i'},
+	{"help", no_argument, 0, 'h'},
+	{ 0, 0, 0, 0 }
+    };
+
+    int opt;
+    int optionIndex = 0;
+    while ((opt = getopt_long(argc, argv, "lih", longOptions, &optionIndex)) != -1) {
+	switch (opt) {
+	    case 'l':
+	        SDRDevice::listAvailableSDRDevices();
+	    exit(0);
+
+	    case 'i':
+	    printDeviceInfo = 1;
+	    break;
+
+	    case 'h':
+	    printHelp();
+	    exit(0);
+	}
+    }
 
     SDRDevice *sdr = new SDRDevice();
 
-    sdr->printInfo();
+    if (printDeviceInfo) {
+	sdr->printInfo();
+    }
 
     delete sdr;
 }
