a98d7e80ad0e7c19ad4b7484b00affc84385620a
The Developer <cubanan@gmail.com>
2016-11-06 11:52:44 +0000

    Added code for printing more device info

--- stdUtils.cpp
+++ stdUtils.cpp
@@ -17,3 +17,22 @@ ostream& operator<< (ostream& os, const Kwargs& kwargs) {
     os << "]";
     return os;
 }
+
+ostream& operator<< (ostream& os, const ArgInfo& argInfo) {
+    os << "[key=" << argInfo.key;
+    os << ",value=" << argInfo.value;
+    os << ",name=" << argInfo.name;
+    os << ",description=" << argInfo.description;
+    os << ",units=" << argInfo.units;
+    os << "]";
+    return os;
+}
+
+ostream& operator<< (ostream& os, const ArgInfoList& argInfoList) {
+    os << "(";
+    for (ArgInfoList::const_iterator iterator = argInfoList.begin(); iterator != argInfoList.end(); iterator++) {
+	os << *iterator << ",";
+    }
+    os << ")";
+    return os;
+}
--- stdUtils.hpp
+++ stdUtils.hpp
@@ -7,3 +7,5 @@ using namespace SoapySDR;
 ostream& operator<< (ostream& os, const vector<string>& values);
 
 ostream& operator<< (ostream& os, const Kwargs& kwargs);
+
+ostream& operator<< (ostream& os, const ArgInfoList& argInfoList);
