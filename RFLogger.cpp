fc5d54f4b4adcfbe48efcbca233f402859217eaf
The Developer <cubanan@gmail.com>
2016-11-10 19:32:36 +0000

    Read in loop mode added

--- RFLogger.cpp
+++ RFLogger.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <iostream>
 #include <getopt.h>
 
@@ -10,27 +11,38 @@ using namespace std;
 static void printHelp() {
     cout << "Usage RFLogger [options]" << endl;
     cout << "  Options summary:" << endl;
-    cout << "    -l, --list-device              List all available SDR devices and exit" << endl;
+    cout << "    -l, --list-device              List all available SDR devices and exit" << endl << endl;
     cout << "    -i, --device-info              Display detailed information about the device in use" << endl;
+    cout << "    -n, --number <number>          Perform <number> reads and exit. If '0' then read in loop." << endl << endl;
     cout << "    -h, --help                     Display this help information and exit" << endl;
+}
+
+static bool keepReading = true;
+
+static void interrupt(int signum) {
+    keepReading = false;
 }
 
 int main (int argc, char **argv) {
 
+    signal(SIGINT, interrupt);
+
     static int printDeviceInfo = 0;
+    static unsigned int number = 10;
 
     cout << "Starting..." << endl;
 
     static struct option longOptions[] = {
-	{"list-devices", no_argument, 0, 'l'},
-	{"device-info", no_argument, &printDeviceInfo, 'i'},
-	{"help", no_argument, 0, 'h'},
-	{ 0, 0, 0, 0 }
+	{ "list-devices", no_argument, 0, 'l' },
+	{ "device-info", no_argument, &printDeviceInfo, 'i' },
+	{ "number", required_argument, 0, 'n' },
+        { "help", no_argument, 0, 'h' },
+        { 0, 0, 0, 0 }
     };
 
     int opt;
     int optionIndex = 0;
-    while ((opt = getopt_long(argc, argv, "lih", longOptions, &optionIndex)) != -1) {
+    while ((opt = getopt_long(argc, argv, "lin:h", longOptions, &optionIndex)) != -1) {
 	switch (opt) {
 	    case 'l':
 	        SDRDevice::listAvailableSDRDevices();
@@ -38,6 +50,10 @@ int main (int argc, char **argv) {
 
 	    case 'i':
 	    printDeviceInfo = 1;
+	    break;
+
+	    case 'n':
+	    number = stoi(optarg);
 	    break;
 
 	    case 'h':
@@ -61,17 +77,30 @@ int main (int argc, char **argv) {
 	sdr->printStreamInfo();
     }
 
-    OStreamSpectrumWriter console(cout, 180);
+    OStreamSpectrumWriter console(cout, 200);
 
     FFT fft;
 
     Samples samples(1 << 16);
     Samples spectrum(samples.size());
 
-    for (unsigned int i = 0; i < 50; i++) {
+    if (number > 0) {
+	cout << "Performing '" << number << "' reads" << endl;
+    } else {
+	cout << "Performing endless reads" << endl;
+    }    
+    
+    unsigned int i = 0;
+    while (keepReading) {
 	sdr->readStream(samples);
 	fft.transform(samples, spectrum);
 	console << spectrum;
+
+	if (number > 0) {
+	    if (++i >= number) {
+		keepReading = false;
+	    }
+	}
     }
 
     sdr->closeStream();
